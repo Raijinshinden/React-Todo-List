ed2a4173cb9c6fe809a5d751a606c405e6070e9e
Ryan Dempsey <rjd9896@gmail.com>
2024-03-21 22:53:31 +0900

    Add files to Repository

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import TodoInput from "./components/TodoInput"
+import TodoList from "./components/TodoList"
+
+
+function App() { 
+  let todos = [
+    'Go to the gym',
+    'Eat more fruits and vege',
+    'Pick up the kids from school'
+  ]
+  return (
+    <>
+      <TodoInput />
+      <TodoList />
+    </>
+  )
+}
+
+export default App
